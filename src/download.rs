7f9c0c53c1cca90ec9a15e5653ffc9ae471f43bb
high-moctane <high.moctane@gmail.com>
2021-10-08 09:52:30 +0900

    WIP

--- src/download.rs
+++ src/download.rs
@@ -1,9 +1,17 @@
-pub fn download() {
+use anyhow::{Context, Result};
+use flate2::read::GzDecoder;
+use std::io::prelude::*;
+
+use reqwest::get;
+
+pub fn download() -> Result<()> {
     for n in 1..=5 {
         for idx in 0..total_files_by_n(n) {
-            println!("{}", file_url(n, idx));
+            download_file(n, idx);
         }
     }
+
+    Ok(())
 }
 
 fn total_files_by_n(n: i8) -> i16 {
@@ -24,4 +32,14 @@ fn file_url(n: i8, idx: i16) -> String {
         "http://storage.googleapis.com/books/ngrams/books/20200217/eng/{}-{:05}-of-{:05}.gz",
         n, idx, max_idx
     )
+}
+
+async fn download_file(n: i8, idx: i16) -> Result<()> {
+    let url = file_url(n, idx);
+
+    let body = reqwest::get(url).await?;
+
+    let mut d: GzDecoder<Box<dyn Read>> = GzDecoder::new(Box::new::<dyn Read>(body.into()));
+
+    Ok(())
 }
