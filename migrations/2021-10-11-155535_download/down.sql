7011ef08e580f459100db4ad393ace6ad09b7b36
high-moctane <high.moctane@gmail.com>
2021-10-12 01:08:43 +0900

    Write DDL

--- migrations/2021-10-11-155535_download/down.sql
+++ migrations/2021-10-11-155535_download/down.sql
@@ -1,1 +1,12 @@
--- This file should undo anything in `up.sql`
+drop table fetched_files;
+drop table five_gram_entries;
+drop table five_grams;
+drop table four_gram_entries;
+drop table four_grams;
+drop table three_gram_entries;
+drop table three_grams;
+drop table two_gram_entries;
+drop table two_grams;
+drop table one_gram_entries;
+drop table one_grams;
+drop table words;
