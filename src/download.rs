643a455db798724d5dcedfe937b0ef999a5195a2
high-moctane <high.moctane@gmail.com>
2021-10-12 23:42:50 +0900

    Embed remote metadata

--- src/download.rs
+++ src/download.rs
@@ -7,6 +7,63 @@ use diesel::sqlite::SqliteConnection;
 use crate::models::*;
 use crate::schema;
 
+#[derive(Debug)]
+enum Language {
+    English,
+    AmericanEnglish,
+    BritishEnglish,
+    EnglishFiction,
+    Chinese,
+    French,
+    German,
+    Hebrew,
+    Italian,
+    Russian,
+    Spanish,
+}
+
+impl Language {
+    fn parse(lang_name: &str) -> Language {
+        match &*lang_name.to_lowercase() {
+            "eng" => Language::English,
+            _ => unimplemented!("not implemented language"),
+        }
+    }
+
+    fn url_name(&self) -> String {
+        match self {
+            Language::English => "eng".to_string(),
+            _ => unimplemented!("not implemented language"),
+        }
+    }
+}
+
+fn total_file_num(lang: &Language, n: i8) -> i16 {
+    match lang {
+        Language::English => match n {
+            1 => 24,
+            2 => 589,
+            3 => 6881,
+            4 => 6668,
+            5 => 19423,
+            _ => panic!("invalid ngram number: {}", n),
+        },
+        _ => unimplemented!("not implemented language: {:?}", lang),
+    }
+}
+
+fn gz_url(lang: &Language, n: i8, idx: i16) -> String {
+    let total = total_file_num(lang, n);
+
+    format!(
+        "http://storage.googleapis.com/books/ngrams/books/20200217/{}/{}-{}-of-{}.gz",
+        lang.url_name(),
+        n,
+        idx,
+        total
+    )
+}
+
 pub fn run() -> Result<()> {
     let conn = SqliteConnection::establish("build/download.sqlite")?;
 
