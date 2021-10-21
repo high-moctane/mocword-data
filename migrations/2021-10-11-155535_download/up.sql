7be7c9eb55eeae968617f4e1e5b4ed569b1efa22
high-moctane <high.moctane@gmail.com>
2021-10-22 02:52:49 +0900

    New schema without entries

--- migrations/2021-10-11-155535_download/up.sql
+++ migrations/2021-10-11-155535_download/up.sql
@@ -6,63 +6,58 @@ create table words (
 create table one_grams (
     id       integer primary key autoincrement,
     word1_id integer not null unique,
+    score    integer not null,
 
-    constraint fk_one_grams_word1_id foreign key (word1_id) references words(id)
+    constraint fk_one_grams_word1_id
+        foreign key (word1_id)
+        references words(id)
+        on delete cascade
 );
 
-create table one_gram_entries (
-    one_gram_id  integer not null,
-    year         integer not null,
-    match_count  integer not null,
-    volume_count integer not null,
-
-    primary key(one_gram_id, year),
-    constraint fk_one_gram_entries_one_gram_id foreign key (one_gram_id) references one_grams(id)
-);
+create index idx_one_grams_score on one_grams(score);
 
 create table two_grams (
     id       integer primary key autoincrement,
     word1_id integer not null,
     word2_id integer not null,
+    score    integer not null,
 
-    constraint fk_two_grams_word1_id foreign key (word1_id) references words(id),
-    constraint fk_two_grams_word2_id foreign key (word2_id) references words(id)
+    constraint fk_two_grams_word1_id
+        foreign key (word1_id)
+        references words(id)
+        on delete cascade,
+    constraint fk_two_grams_word2_id
+        foreign key (word2_id)
+        references words(id)
+        on delete cascade
 );
 
 create unique index idx_two_grams on two_grams(word1_id, word2_id);
-
-create table two_gram_entries (
-    two_gram_id  integer not null,
-    year         integer not null,
-    match_count  integer not null,
-    volume_count integer not null,
-
-    primary key(two_gram_id, year),
-    constraint fk_two_gram_entries_two_gram_id foreign key (two_gram_id) references two_grams(id)
-);
+create index idx_two_grams_score on two_grams(score);
 
 create table three_grams (
     id       integer primary key autoincrement,
     word1_id integer not null,
     word2_id integer not null,
     word3_id integer not null,
+    score    integer not null,
 
-    constraint fk_three_grams_word1_id foreign key (word1_id) references words(id),
-    constraint fk_three_grams_word2_id foreign key (word2_id) references words(id),
-    constraint fk_three_grams_word3_id foreign key (word3_id) references words(id)
+    constraint fk_three_grams_word1_id
+        foreign key (word1_id)
+        references words(id)
+        on delete cascade,
+    constraint fk_three_grams_word2_id
+        foreign key (word2_id)
+        references words(id)
+        on delete cascade,
+    constraint fk_three_grams_word3_id
+        foreign key (word3_id)
+        references words(id)
+        on delete cascade
 );
 
 create unique index idx_three_grams on three_grams(word1_id, word2_id, word3_id);
-
-create table three_gram_entries (
-    three_gram_id integer not null,
-    year          integer not null,
-    match_count   integer not null,
-    volume_count  integer not null,
-
-    primary key(three_gram_id, year),
-    constraint fk_three_gram_entries_three_gram_id foreign key (three_gram_id) references three_grams(id)
-);
+create index idx_three_grams_score on three_grams(score);
 
 create table four_grams (
     id       integer primary key autoincrement,
@@ -70,24 +65,28 @@ create table four_grams (
     word2_id integer not null,
     word3_id integer not null,
     word4_id integer not null,
+    score    integer not null,
 
-    constraint fk_four_grams_word1_id foreign key (word1_id) references words(id),
-    constraint fk_four_grams_word2_id foreign key (word2_id) references words(id),
-    constraint fk_four_grams_word3_id foreign key (word3_id) references words(id),
-    constraint fk_four_grams_word4_id foreign key (word4_id) references words(id)
+    constraint fk_four_grams_word1_id
+        foreign key (word1_id)
+        references words(id)
+        on delete cascade,
+    constraint fk_four_grams_word2_id
+        foreign key (word2_id)
+        references words(id)
+        on delete cascade,
+    constraint fk_four_grams_word3_id
+        foreign key (word3_id)
+        references words(id)
+        on delete cascade,
+    constraint fk_four_grams_word4_id
+        foreign key (word4_id)
+        references words(id)
+        on delete cascade
 );
 
 create unique index idx_four_grams on four_grams(word1_id, word2_id, word3_id, word4_id);
-
-create table four_gram_entries (
-    four_gram_id integer not null,
-    year         integer not null,
-    match_count  integer not null,
-    volume_count integer not null,
-
-    primary key(four_gram_id, year),
-    constraint fk_four_gram_entries_four_gram_id foreign key (four_gram_id) references four_grams(id)
-);
+create index idx_four_grams_score on four_grams(score);
 
 create table five_grams (
     id       integer primary key autoincrement,
@@ -96,25 +95,32 @@ create table five_grams (
     word3_id integer not null,
     word4_id integer not null,
     word5_id integer not null,
+    score    integer not null,
 
-    constraint fk_five_grams_word1_id foreign key (word1_id) references words(id),
-    constraint fk_five_grams_word2_id foreign key (word2_id) references words(id),
-    constraint fk_five_grams_word3_id foreign key (word3_id) references words(id),
-    constraint fk_five_grams_word4_id foreign key (word4_id) references words(id),
-    constraint fk_five_grams_word5_id foreign key (word5_id) references words(id)
+    constraint fk_five_grams_word1_id
+        foreign key (word1_id)
+        references words(id)
+        on delete cascade,
+    constraint fk_five_grams_word2_id
+        foreign key (word2_id)
+        references words(id)
+        on delete cascade,
+    constraint fk_five_grams_word3_id
+        foreign key (word3_id)
+        references words(id)
+        on delete cascade,
+    constraint fk_five_grams_word4_id
+        foreign key (word4_id)
+        references words(id)
+        on delete cascade,
+    constraint fk_five_grams_word5_id
+        foreign key (word5_id)
+        references words(id)
+        on delete cascade
 );
 
 create unique index idx_five_grams on five_grams(word1_id, word2_id, word3_id, word4_id, word5_id);
-
-create table five_gram_entries (
-    five_gram_id integer not null,
-    year         integer not null,
-    match_count  integer not null,
-    volume_count integer not null,
-
-    primary key(five_gram_id, year),
-    constraint fk_five_gram_entries_five_gram_id foreign key (five_gram_id) references five_grams(id)
-);
+create index idx_five_grams_score on four_grams(score);
 
 create table fetched_files (
     n   integer not null,
