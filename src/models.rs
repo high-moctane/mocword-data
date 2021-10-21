f12fef4dd7e6ad601859a7c13dfa3790ec35cf54
high-moctane <high.moctane@gmail.com>
2021-10-22 02:52:49 +0900

    New schema without entries

--- src/models.rs
+++ src/models.rs
@@ -1,10 +1,9 @@
 use crate::schema::{
-    fetched_files, five_gram_entries, five_grams, four_gram_entries, four_grams, one_gram_entries,
-    one_grams, three_gram_entries, three_grams, two_gram_entries, two_grams, words,
+    fetched_files, five_grams, four_grams, one_grams, three_grams, two_grams, words,
 };
 
 pub trait Ngram {
-    fn n(&self) -> i16;
+    fn n(&self) -> i64;
     fn get_id(&self) -> i64;
 }
 
@@ -24,10 +23,11 @@ pub struct NewWord {
 pub struct OneGram {
     pub id: i64,
     pub word1_id: i64,
+    pub score: i64,
 }
 
 impl Ngram for OneGram {
-    fn n(&self) -> i16 {
+    fn n(&self) -> i64 {
         1
     }
 
@@ -40,23 +40,7 @@ impl Ngram for OneGram {
 #[table_name = "one_grams"]
 pub struct NewOneGram {
     pub word1_id: i64,
-}
-
-#[derive(Queryable)]
-pub struct OneGramEntry {
-    pub one_gram_id: i64,
-    pub year: i16,
-    pub match_count: i64,
-    pub volume_count: i64,
-}
-
-#[derive(Insertable)]
-#[table_name = "one_gram_entries"]
-pub struct NewOneGramEntry {
-    pub one_gram_id: i64,
-    pub year: i16,
-    pub match_count: i64,
-    pub volume_count: i64,
+    pub score: i64,
 }
 
 #[derive(Queryable, Copy, Clone)]
@@ -64,10 +48,11 @@ pub struct TwoGram {
     pub id: i64,
     pub word1_id: i64,
     pub word2_id: i64,
+    pub score: i64,
 }
 
 impl Ngram for TwoGram {
-    fn n(&self) -> i16 {
+    fn n(&self) -> i64 {
         2
     }
 
@@ -81,23 +66,7 @@ impl Ngram for TwoGram {
 pub struct NewTwoGram {
     pub word1_id: i64,
     pub word2_id: i64,
-}
-
-#[derive(Queryable)]
-pub struct TwoGramEntry {
-    pub two_gram_id: i64,
-    pub year: i16,
-    pub match_count: i64,
-    pub volume_count: i64,
-}
-
-#[derive(Insertable)]
-#[table_name = "two_gram_entries"]
-pub struct NewTwoGramEntry {
-    pub two_gram_id: i64,
-    pub year: i16,
-    pub match_count: i64,
-    pub volume_count: i64,
+    pub score: i64,
 }
 
 #[derive(Queryable, Copy, Clone)]
@@ -106,10 +75,11 @@ pub struct ThreeGram {
     pub word1_id: i64,
     pub word2_id: i64,
     pub word3_id: i64,
+    pub score: i64,
 }
 
 impl Ngram for ThreeGram {
-    fn n(&self) -> i16 {
+    fn n(&self) -> i64 {
         3
     }
 
@@ -124,23 +94,7 @@ pub struct NewThreeGram {
     pub word1_id: i64,
     pub word2_id: i64,
     pub word3_id: i64,
-}
-
-#[derive(Queryable)]
-pub struct ThreeGramEntry {
-    pub three_gram_id: i64,
-    pub year: i16,
-    pub match_count: i64,
-    pub volume_count: i64,
-}
-
-#[derive(Insertable)]
-#[table_name = "three_gram_entries"]
-pub struct NewThreeGramEntry {
-    pub three_gram_id: i64,
-    pub year: i16,
-    pub match_count: i64,
-    pub volume_count: i64,
+    pub score: i64,
 }
 
 #[derive(Queryable, Copy, Clone)]
@@ -150,10 +104,11 @@ pub struct FourGram {
     pub word2_id: i64,
     pub word3_id: i64,
     pub word4_id: i64,
+    pub score: i64,
 }
 
 impl Ngram for FourGram {
-    fn n(&self) -> i16 {
+    fn n(&self) -> i64 {
         4
     }
 
@@ -169,23 +124,7 @@ pub struct NewFourGram {
     pub word2_id: i64,
     pub word3_id: i64,
     pub word4_id: i64,
-}
-
-#[derive(Queryable)]
-pub struct FourGramEntry {
-    pub four_gram_id: i64,
-    pub year: i16,
-    pub match_count: i64,
-    pub volume_count: i64,
-}
-
-#[derive(Insertable)]
-#[table_name = "four_gram_entries"]
-pub struct NewFourGramEntry {
-    pub four_gram_id: i64,
-    pub year: i16,
-    pub match_count: i64,
-    pub volume_count: i64,
+    pub score: i64,
 }
 
 #[derive(Queryable, Copy, Clone)]
@@ -196,10 +135,11 @@ pub struct FiveGram {
     pub word3_id: i64,
     pub word4_id: i64,
     pub word5_id: i64,
+    pub score: i64,
 }
 
 impl Ngram for FiveGram {
-    fn n(&self) -> i16 {
+    fn n(&self) -> i64 {
         5
     }
 
@@ -216,34 +156,18 @@ pub struct NewFiveGram {
     pub word3_id: i64,
     pub word4_id: i64,
     pub word5_id: i64,
-}
-
-#[derive(Queryable)]
-pub struct FiveGramEntry {
-    pub five_gram_id: i64,
-    pub year: i16,
-    pub match_count: i64,
-    pub volume_count: i64,
-}
-
-#[derive(Insertable)]
-#[table_name = "five_gram_entries"]
-pub struct NewFiveGramEntry {
-    pub five_gram_id: i64,
-    pub year: i16,
-    pub match_count: i64,
-    pub volume_count: i64,
+    pub score: i64,
 }
 
 #[derive(Queryable)]
 pub struct FetchedFile {
-    pub n: i16,
-    pub idx: i16,
+    pub n: i64,
+    pub idx: i64,
 }
 
 #[derive(Insertable)]
 #[table_name = "fetched_files"]
 pub struct NewFetchedFile {
-    pub n: i16,
-    pub idx: i16,
+    pub n: i64,
+    pub idx: i64,
 }
