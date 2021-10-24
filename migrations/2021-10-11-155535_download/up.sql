a44ac7673dfce2a78ee9dc6f63547f7749703354
high-moctane <high.moctane@gmail.com>
2021-10-24 12:30:50 +0900

    Simplify schema

--- migrations/2021-10-11-155535_download/up.sql
+++ migrations/2021-10-11-155535_download/up.sql
@@ -1,111 +1,41 @@
-create table words (
-    id   integer  primary key autoincrement,
-    word text     not null unique
-);
-
 create table one_grams (
-    id       integer primary key autoincrement,
-    word1_id integer not null,
-    score    integer not null,
-
-    constraint fk_one_grams_word1_id
-        foreign key (word1_id)
-        references words(id)
-        on delete cascade
+    id       integer primary key,
+    word     text    not null,
+    score    integer not null
 );
 
 create table two_grams (
-    id       integer primary key autoincrement,
+    id       integer primary key,
     word1_id integer not null,
     word2_id integer not null,
-    score    integer not null,
-
-    constraint fk_two_grams_word1_id
-        foreign key (word1_id)
-        references words(id)
-        on delete cascade,
-    constraint fk_two_grams_word2_id
-        foreign key (word2_id)
-        references words(id)
-        on delete cascade
+    score    integer not null
 );
 
 create table three_grams (
-    id       integer primary key autoincrement,
+    id       integer primary key,
     word1_id integer not null,
     word2_id integer not null,
     word3_id integer not null,
-    score    integer not null,
-
-    constraint fk_three_grams_word1_id
-        foreign key (word1_id)
-        references words(id)
-        on delete cascade,
-    constraint fk_three_grams_word2_id
-        foreign key (word2_id)
-        references words(id)
-        on delete cascade,
-    constraint fk_three_grams_word3_id
-        foreign key (word3_id)
-        references words(id)
-        on delete cascade
+    score    integer not null
 );
 
 create table four_grams (
-    id       integer primary key autoincrement,
+    id       integer primary key,
     word1_id integer not null,
     word2_id integer not null,
     word3_id integer not null,
     word4_id integer not null,
-    score    integer not null,
-
-    constraint fk_four_grams_word1_id
-        foreign key (word1_id)
-        references words(id)
-        on delete cascade,
-    constraint fk_four_grams_word2_id
-        foreign key (word2_id)
-        references words(id)
-        on delete cascade,
-    constraint fk_four_grams_word3_id
-        foreign key (word3_id)
-        references words(id)
-        on delete cascade,
-    constraint fk_four_grams_word4_id
-        foreign key (word4_id)
-        references words(id)
-        on delete cascade
+    score    integer not null
 );
 
 create table five_grams (
-    id       integer primary key autoincrement,
+    id       integer primary key,
     word1_id integer not null,
     word2_id integer not null,
     word3_id integer not null,
     word4_id integer not null,
     word5_id integer not null,
-    score    integer not null,
-
-    constraint fk_five_grams_word1_id
-        foreign key (word1_id)
-        references words(id)
-        on delete cascade,
-    constraint fk_five_grams_word2_id
-        foreign key (word2_id)
-        references words(id)
-        on delete cascade,
-    constraint fk_five_grams_word3_id
-        foreign key (word3_id)
-        references words(id)
-        on delete cascade,
-    constraint fk_five_grams_word4_id
-        foreign key (word4_id)
-        references words(id)
-        on delete cascade,
-    constraint fk_five_grams_word5_id
-        foreign key (word5_id)
-        references words(id)
-        on delete cascade
+    score    integer not null
 );
 
 create table fetched_files (
