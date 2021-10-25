42f0027cc7f4e0900c226b84779f1ae30ba29edb
high-moctane <high.moctane@gmail.com>
2021-10-26 00:11:31 +0900

    Impl parse_args

--- src/download.rs
+++ src/download.rs
@@ -1,6 +1,15 @@
+use std::path::Path;
+
 use anyhow::Result;
+use clap::{App, Arg, SubCommand};
 use env_logger;
-use log::info;
+use log::{debug, info};
+
+#[derive(Debug)]
+struct Args {
+    lang: String,
+    dir: String,
+}
 
 pub fn run() -> Result<()> {
     env_logger::init();
@@ -13,7 +22,38 @@ pub fn run() -> Result<()> {
     Ok(())
 }
 
-fn parse_args() {}
+fn parse_args() -> Args {
+    let languages = vec!["eng"];
+
+    let matches = App::new("Mocword Download")
+        .author("high-moctane <high.moctane@gmail.com>")
+        .about("Download and build mocword ngram data")
+        .arg(
+            Arg::with_name("language")
+                .short("l")
+                .long("lang")
+                .value_name("LANG")
+                .help(format!("Sets a language (default: eng): {}", languages.join(", ")).as_str())
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("dir")
+                .long("dir")
+                .short("d")
+                .value_name("DIR")
+                .help("Destination directory path")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let lang = matches
+        .value_of("language")
+        .unwrap_or_else(|| "eng")
+        .to_string();
+    let dir = matches.value_of("dir").unwrap_or_else(|| ".").to_string();
+
+    Args { lang, dir }
+}
 
 fn migrate() {}
 
