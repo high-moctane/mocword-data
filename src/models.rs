94a236d03edc0eac288e0ef1e573b15be63bc510
high-moctane <high.moctane@gmail.com>
2021-11-15 01:42:56 +0900

    wip

--- src/models.rs
+++ src/models.rs
@@ -1,4 +1,4 @@
-use crate::schema::fetched_files;
+use crate::schema::{fetched_files, one_gram_scores};
 
 #[derive(Queryable, Clone)]
 pub struct FetchedFile {
@@ -11,4 +11,18 @@ pub struct FetchedFile {
 pub struct NewFetchedFile {
     pub n: i64,
     pub idx: i64,
+}
+
+#[derive(Queryable, Clone)]
+pub struct OneGramScore {
+    pub id: i64,
+    pub word: String,
+    pub score: i64,
+}
+
+#[derive(Insertable, Debug)]
+#[table_name = "one_gram_scores"]
+pub struct NewOneGramScore {
+    pub word: String,
+    pub score: i64,
 }
